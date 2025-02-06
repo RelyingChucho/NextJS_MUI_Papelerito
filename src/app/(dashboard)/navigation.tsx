62bbf4fbe001929cb1232fda7494c0ae2179bcd7
Jesus Alberto <albertofw70@gmail.com>
2025-02-06 11:27:32 -0600

    Añadir navegación y nuevos íconos para categorías, marcas y productos en el dashboard

--- src/app/(dashboard)/navigation.tsx
+++ src/app/(dashboard)/navigation.tsx
@@ -1,5 +1,8 @@
 import CategoryRoundedIcon from "@mui/icons-material/CategoryRounded";
 import { type Navigation } from "@toolpad/core/AppProvider";
+import { Category, AddCategory, ListSearch, ListAdd } from "@/icons";
+import StoreRoundedIcon from "@mui/icons-material/StoreRounded";
+import AddBusinessRoundedIcon from "@mui/icons-material/AddBusinessRounded";
 
 export const NAVIGATION: Navigation = [
   {
@@ -9,7 +12,48 @@ export const NAVIGATION: Navigation = [
   {
     segment: "Categorias",
     title: "Categorias",
-    icon: <CategoryRoundedIcon />
-    ,
+    icon: <Category />,
+    children: [
+      {
+        segment: "Registrar",
+        title: "Registrar Categoria",
+        icon: <AddCategory />,
+      },
+      {
+        segment: "",
+        title: "Consultar Categorias",
+        icon: <ListSearch />,
+      },
+    ],
+  },
+  {
+    segment: "Marcas",
+    title: "Marcas",
+    icon: <StoreRoundedIcon />,
+    children: [
+      {
+        segment: "Registrar",
+        title: "Registrar Marca",
+        icon: <AddBusinessRoundedIcon />,
+      },
+      { segment: "", title: "Consultar Marcas", icon: <ListSearch /> },
+    ],
+  },
+  {
+    segment: "Productos",
+    title: "Productos",
+    icon: <CategoryRoundedIcon />,
+    children: [
+      {
+        segment: "Registrar",
+        title: "Registrar Productos",
+        icon: <ListAdd />,
+      },
+      {
+        segment: "",
+        title: "Consultar Productos",
+        icon: <ListSearch />,
+      },
+    ],
   },
 ];
---
+++ src/icons.js
@@ -0,0 +1,91 @@
+export function Category() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-category"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 4h6v6h-6z" />
+      <path d="M14 4h6v6h-6z" />
+      <path d="M4 14h6v6h-6z" />
+      <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+    </svg>
+  );
+}
+
+export function AddCategory() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-category-plus"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 4h6v6h-6zm10 0h6v6h-6zm-10 10h6v6h-6zm10 3h6m-3 -3v6" />
+    </svg>
+  );
+}
+
+export function ListSearch() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-list-search"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M15 15m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+      <path d="M18.5 18.5l2.5 2.5" />
+      <path d="M4 6h16" />
+      <path d="M4 12h4" />
+      <path d="M4 18h4" />
+    </svg>
+  );
+}
+
+export function ListAdd() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-text-plus"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M19 10h-14" />
+      <path d="M5 6h14" />
+      <path d="M14 14h-9" />
+      <path d="M5 18h6" />
+      <path d="M18 15v6" />
+      <path d="M15 18h6" />
+    </svg>
+  );
+}
