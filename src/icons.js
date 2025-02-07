de49e4d357f24cb24bc08f89ae0bbfdc2e525a21
Jesus Alberto Martinez Santos <albertofw70@gmail.com>
2025-02-07 00:07:56 -0600

    Refactor componentes de iconos para utilizar SvgIcon de MUI

--- src/icons.js
+++ src/icons.js
@@ -1,91 +1,120 @@
+// "use client";
+
+import { createSvgIcon, SvgIcon } from "@mui/material";
+
+// export const Category = createSvgIcon(
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     width="24"
+//     height="24"
+//     viewBox="0 0 24 24"
+//     fill="none"
+//     stroke="currentColor"
+//     strokeWidth="2"
+//     strokeLinecap="round"
+//     strokeLinejoin="round"
+//   >
+//     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+//     <path d="M4 4h6v6h-6z" />
+//     <path d="M14 4h6v6h-6z" />
+//     <path d="M4 14h6v6h-6z" />
+//     <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+//   </svg>,
+//   " Category"
+// );
+
 export function Category() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-category"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M4 4h6v6h-6z" />
-      <path d="M14 4h6v6h-6z" />
-      <path d="M4 14h6v6h-6z" />
-      <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-    </svg>
+    <SvgIcon>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 4h6v6h-6z" />
+        <path d="M14 4h6v6h-6z" />
+        <path d="M4 14h6v6h-6z" />
+        <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      </svg>
+    </SvgIcon>
   );
 }
 
 export function AddCategory() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-category-plus"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M4 4h6v6h-6zm10 0h6v6h-6zm-10 10h6v6h-6zm10 3h6m-3 -3v6" />
-    </svg>
+    <SvgIcon>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 4h6v6h-6zm10 0h6v6h-6zm-10 10h6v6h-6zm10 3h6m-3 -3v6" />
+      </svg>
+    </SvgIcon>
   );
 }
 
 export function ListSearch() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-list-search"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M15 15m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-      <path d="M18.5 18.5l2.5 2.5" />
-      <path d="M4 6h16" />
-      <path d="M4 12h4" />
-      <path d="M4 18h4" />
-    </svg>
+    <SvgIcon>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M15 15m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+        <path d="M18.5 18.5l2.5 2.5" />
+        <path d="M4 6h16" />
+        <path d="M4 12h4" />
+        <path d="M4 18h4" />
+      </svg>
+    </SvgIcon>
   );
 }
 
 export function ListAdd() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-text-plus"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M19 10h-14" />
-      <path d="M5 6h14" />
-      <path d="M14 14h-9" />
-      <path d="M5 18h6" />
-      <path d="M18 15v6" />
-      <path d="M15 18h6" />
-    </svg>
+    <SvgIcon>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M19 10h-14" />
+        <path d="M5 6h14" />
+        <path d="M14 14h-9" />
+        <path d="M5 18h6" />
+        <path d="M18 15v6" />
+        <path d="M15 18h6" />
+      </svg>
+    </SvgIcon>
   );
 }
