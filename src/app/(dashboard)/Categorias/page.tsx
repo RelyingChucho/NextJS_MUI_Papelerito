36014e905bdac29bf39ef485aebee87211709cdb
Jesus Alberto Martinez Santos <albertofw70@gmail.com>
2025-02-06 20:30:19 -0600

    Añadir metadatos para la página de categorías, incluyendo título, descripción y autor

--- src/app/(dashboard)/Categorias/page.tsx
+++ src/app/(dashboard)/Categorias/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Categorias",
+  description: "En esta parte se Consultan las Categorias",
+  authors: [{ name: "Jesus Alberto Martinez Santos" }],
+  icons: "/papeleria.ico",
+};
+
 export default function Page() {
   return <div>Hola desde Categorias</div>;
 }
