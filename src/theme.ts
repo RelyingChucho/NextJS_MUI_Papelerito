e3877cc04497e4a175a2ef88667228879f94b048
Jesus Alberto Martinez Santos <albertofw70@gmail.com>
2025-02-06 20:57:27 -0600

    Añadir esquemas de color personalizados para los modos claro y oscuro en el tema

--- src/theme.ts
+++ src/theme.ts
@@ -3,8 +3,22 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   colorSchemes: {
-    dark: true,
-    light: true,
+    light: {
+      palette: {
+        background: {
+          default: "#F3F4F6",
+          paper: "#FFFEFE",
+        },
+      },
+    },
+    dark: {
+      palette: {
+        background: {
+          default: "#252728",
+          paper: "#1D1C1C",
+        },
+      },
+    },
   },
   cssVariables: {
     colorSchemeSelector: "data-toolpad-color-scheme",
