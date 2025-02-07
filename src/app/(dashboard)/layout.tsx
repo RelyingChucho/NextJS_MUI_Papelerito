3f26b52674285bf07d6b36d30f6992f31871f5b4
Jesus Alberto Martinez Santos <albertofw70@gmail.com>
2025-02-07 00:56:31 -0600

    Añadir soporte para carga diferida en el layout del dashboard y optimizar la importación de imágenes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,54 +3,60 @@ import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import ThemeToggle from "@/themeToggle";
 import { NextAppProvider } from "@toolpad/core/nextjs";
 import { NAVIGATION } from "@/app/(dashboard)/navigation";
-import { Typography } from "@mui/material";
+import { Skeleton, Typography } from "@mui/material";
+import Image from "next/image";
+import { Suspense } from "react";
 
 export default function DashboardPagesLayout(props: {
   children: React.ReactNode;
 }) {
   return (
-    <NextAppProvider
-      navigation={NAVIGATION}
-      branding={{
-        logo: <img src="/papeleria.gif" />,
-        homeUrl: "/",
-        title: (
-          <Typography className="text-teal-600 dark:text-white font-cormorantGaramond text-xl sm:text-2xl md:text-3xl font-bold">
-            EL GRAN PAPELERITO
-          </Typography>
-        ),
-      }}
+    <Suspense
+      fallback={<Skeleton variant="rounded" width={"100%"} height={"100%"} />}
     >
-      <DashboardLayout
-        slots={{ toolbarActions: ThemeToggle }}
-        sidebarExpandedWidth={275}
-        sx={{
-          // Estilos para los ítems del menú
-          ".MuiListItemButton-root": {
-            "&:hover": {
-              backgroundColor: "#0f766e", // Color al pasar el mouse
-              color: "#FFFFFF",
-              ".MuiSvgIcon-root": {
-                color: "#FFFFFF",
-              },
-            },
-            "&.Mui-selected": {
-              backgroundColor: "#0d9488", // Color cuando el ítem está seleccionado
-              ".MuiSvgIcon-root": {
-                color: "#FFFFFF",
-              },
-              ".MuiTypography-root": {
-                color: "#FFFFFF",
-              },
-            },
-            "&.Mui-selected:hover": {
-              backgroundColor: "#2dd4bf", // Color cuando el ítem seleccionado tiene hover
-            },
-          },
+      <NextAppProvider
+        navigation={NAVIGATION}
+        branding={{
+          logo: <Image src="/papeleria.gif" alt="" width={40} height={40} unoptimized/>,
+          homeUrl: "/",
+          title: (
+            <Typography className="text-teal-600 dark:text-white font-cormorantGaramond text-xl sm:text-2xl md:text-3xl font-bold">
+              EL GRAN PAPELERITO
+            </Typography>
+          ) as unknown as string,
         }}
       >
-        <div className="p-5 h-full w-full">{props.children}</div>
-      </DashboardLayout>
-    </NextAppProvider>
+        <DashboardLayout
+          slots={{ toolbarActions: ThemeToggle }}
+          sidebarExpandedWidth={275}
+          sx={{
+            // Estilos para los ítems del menú
+            ".MuiListItemButton-root": {
+              "&:hover": {
+                backgroundColor: "#0f766e", // Color al pasar el mouse
+                color: "#FFFFFF",
+                ".MuiSvgIcon-root": {
+                  color: "#FFFFFF",
+                },
+              },
+              "&.Mui-selected": {
+                backgroundColor: "#0d9488", // Color cuando el ítem está seleccionado
+                ".MuiSvgIcon-root": {
+                  color: "#FFFFFF",
+                },
+                ".MuiTypography-root": {
+                  color: "#FFFFFF",
+                },
+              },
+              "&.Mui-selected:hover": {
+                backgroundColor: "#2dd4bf", // Color cuando el ítem seleccionado tiene hover
+              },
+            },
+          }}
+        >
+          <div className="p-5 h-full w-full">{props.children}</div>
+        </DashboardLayout>
+      </NextAppProvider>
+    </Suspense>
   );
 }
