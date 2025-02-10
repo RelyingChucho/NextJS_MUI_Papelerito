7b52a3a402154e87718fb49e3304c9da2099c6d0
Jesus Alberto <albertofw70@gmail.com>
2025-02-10 15:53:30 -0600

    Añadir soporte para carga diferida en el layout de categorías y renderizar datos con VirtualizedTable

--- src/app/(dashboard)/Categorias/layout.tsx
+++ src/app/(dashboard)/Categorias/layout.tsx
@@ -1,5 +1,12 @@
+import { Skeleton } from "@mui/material";
+import { Suspense } from "react";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-full w-full">{children}</div>
+    <Suspense
+      fallback={<Skeleton variant="rounded" width={"100%"} height={"100%"} />}
+    >
+      <div className="h-full w-full">{children}</div>
+    </Suspense>
   );
 }
--- src/app/(dashboard)/Categorias/page.tsx
+++ src/app/(dashboard)/Categorias/page.tsx
@@ -1,10 +1,44 @@
+import { ColumnData, VirtualizedTable } from "@/components/VirtualizedTable";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Categorias",
-  description: "En esta parte se Consultan las Categorias"
+  description: "En esta parte se Consultan las Categorias",
 };
 
-export default function Page() {
-  return <div>Hola desde Categorias</div>;
+// 1. Define tu tipo de datos
+interface Categorias {
+  id: string;
+  nombre: string;
+  atributos: string[];
+}
+
+// 2. Define las columnas
+const categoriasColumns: ColumnData<Categorias>[] = [
+  {
+    dataKey: "nombre",
+    label: "Nombre",
+    width: 200,
+  },
+  {
+    dataKey: "atributos",
+    label: "Atributos",
+    width: 150,
+  },
+];
+
+
+export default async function Page() {
+  const data = await fetch("http://192.168.100.241:3000/api/Categorias");
+
+  const categorias: Categorias[] = await data.json();
+
+  console.log(categorias);
+
+  return (
+    <VirtualizedTable<Categorias>
+      columns={categoriasColumns}
+      data={categorias}
+    />
+  );
 }
