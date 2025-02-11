509f21b4667962023f228cfc1676b9c320008699
Jesus Alberto <albertofw70@gmail.com>
2025-02-11 14:25:35 -0600

    Añadir componente CustomSelect para ordenar categorías y mejorar el layout del componente

--- src/app/(dashboard)/Categorias/layout.tsx
+++ src/app/(dashboard)/Categorias/layout.tsx
@@ -1,16 +1,24 @@
 import Input from "@/components/Input";
+import CustomSelect, { Option } from "@/components/OrdenarPor";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const ordenarOptions: Option[] = [
+    { value: "asc", label: "Ascendente", param: "ordenNombre" },
+    { value: "desc", label: "Descendente", param: "ordenNombre" },
+  ];
   return (
     <>
       <div className="w-full h-full flex flex-col gap-5 items-center">
-        <Input
-          variant="outlined"
-          label="Nombre Categoria"
-          className="w-64"
-          param
-          paramName="nombre"
-        />
+        <div className="w-full h-fit flex flex-row flex-wrap justify-evenly">
+          <Input
+            variant="outlined"
+            label="Nombre Categoria"
+            className="w-64"
+            param
+            paramName="nombre"
+          />
+          <CustomSelect options={ordenarOptions} label="Ordenar Por" param />
+        </div>
         <div className="h-full w-full">{children}</div>
       </div>
     </>
