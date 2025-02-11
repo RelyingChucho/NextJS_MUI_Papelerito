c7e826a517dd2fa9a4777ac6bab5a39da4147629
Jesus Alberto <albertofw70@gmail.com>
2025-02-11 16:50:04 -0600

    Añadir componente DeleteParams para eliminar filtros en la interfaz de usuario

---
+++ src/components/DeleteParams.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter, usePathname } from "next/navigation";
+import { Button, IconButton, Tooltip } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function DeleteParams() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleDeleteParams = () => {
+    // Navega a la misma ruta sin los parámetros de búsqueda
+    router.push(pathname);
+  };
+
+  return (
+    // {/* <IconButton
+    //     aria-label="delete"
+    //     onClick={handleDeleteParams}
+    //     className="text-teal-600"
+    //   >
+    //     <DeleteIcon />
+    //   </IconButton> */}
+    <Button
+      variant="outlined"
+      startIcon={<DeleteIcon />}
+      onClick={handleDeleteParams}
+      className="rounded-full h-10 border-teal-600 text-white bg-teal-600 hover:scale-110 transition-transform duration-300"
+    >
+      Borrar Filtros
+    </Button>
+  );
+}
