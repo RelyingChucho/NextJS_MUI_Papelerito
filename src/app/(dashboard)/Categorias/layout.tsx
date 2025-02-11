684df40f7e3f736303629dda25df9ea589315872
Jesus Alberto <albertofw70@gmail.com>
2025-02-11 15:52:15 -0600

    Mejorar el diseño del layout de categorías ajustando el espaciado en la disposición de los elementos

--- src/app/(dashboard)/Categorias/layout.tsx
+++ src/app/(dashboard)/Categorias/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="w-full h-full flex flex-col gap-5 items-center">
-        <div className="w-full h-fit flex flex-row flex-wrap justify-evenly">
+        <div className="w-full h-fit flex flex-row md:flex-wrap justify-evenly gap-5">
           <Input
             variant="outlined"
             label="Nombre Categoria"
