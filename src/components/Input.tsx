fa842651d01106c158a82a365d928848ce46a4e6
Jesus Alberto <albertofw70@gmail.com>
2025-02-11 10:59:58 -0600

    Añadir funcionalidad de debounce y manejo de parámetros de búsqueda en el componente de entrada

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,26 +1,80 @@
+"use client";
+
 import { TextField, TextFieldProps } from "@mui/material";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState, useRef } from "react";
 
 const diseño = {
+  "& label.Mui-focused": {
+    color: "#0d9488",
+  },
   "& label": {
-    color: "#94B3F2",
+    color: "#0d9488",
   },
   "& .MuiInput-underline:after": {
-    borderBottomColor: "#94B3F2",
+    borderBottomColor: "#0d9488",
   },
   "& .MuiOutlinedInput-root": {
     "& fieldset": {
-      borderColor: "#94B3F2",
+      borderColor: "#0d9488",
     },
     "&:hover fieldset": {
-      borderColor: "#94B3F2",
+      borderColor: "#0d9488",
     },
     "&.Mui-focused fieldset": {
-      borderColor: "#94B3F2",
+      borderColor: "#0d9488",
     },
   },
-  caretColor: "#94B3F2",
+  caretColor: "#0d9488",
 };
 
-export default function Input({ ...props }: TextFieldProps) {
-  return <TextField {...props} sx={diseño} />;
+type InputProps = TextFieldProps & {
+  paramName?: string;
+  param?: boolean;
+};
+
+export default function Input({
+  paramName = "",
+  param = false,
+  ...props
+}: InputProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [value, setValue] = useState("");
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  // Obtener valor inicial de los searchParams
+  useEffect(() => {
+    if (param) {
+      const initialValue = searchParams.get(paramName) || "";
+      setValue(initialValue);
+    }
+  }, [paramName, searchParams, param]);
+
+  // Actualizar searchParams con debounce
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+
+    if (param) {
+      if (debounceTimeout.current) {
+        clearTimeout(debounceTimeout.current);
+      }
+
+      debounceTimeout.current = setTimeout(() => {
+        const params = new URLSearchParams(searchParams.toString());
+        if (newValue) {
+          params.set(paramName, newValue);
+        } else {
+          params.delete(paramName);
+        }
+
+        router.replace(`?${params.toString()}`, { scroll: false });
+      }, 300);
+    }
+  };
+
+  return (
+    <TextField {...props} value={value} onChange={handleChange} sx={diseño} />
+  );
 }
